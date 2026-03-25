ca53fba6bf1f600bc06e18c1860aa740623f16ef
Vaaris Chitkara <vaarischitkara16@gmail.com>
2026-03-25 11:02:01 +0530

    changes to backend there are some errors

---
+++ backend/migrations/001_subscribers_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE subscribers (
+  id UUID PRIMARY KEY,
+  email TEXT NOT NULL UNIQUE,
+  name TEXT NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,14 +1,41 @@
-use sqlx::Row;
 use std::error::Error;
+use uuid::Uuid;
+
+struct Sub {
+    pub email: String,
+    pub name: String,
+}
+
+async fn create_subscriber(
+    subscriber_data: &Sub,
+    pool: &sqlx::PgPool,
+) -> Result<(), Box<dyn Error>> {
+    let query = "INSERT INTO subscribers (id, email, name) VALUES ($1, $2, $3)";
+    let id = Uuid::new_v4();
+
+    sqlx::query(query)
+        .bind(&id)
+        .bind(&subscriber_data.email)
+        .bind(&subscriber_data.name)
+        .execute(pool)
+        .await?;
+
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let url = "postgres://vaarischitkara:vardan22@localhost:5432/postgres";
     let pool = sqlx::postgres::PgPool::connect(url).await?;
 
-    let res = sqlx::query("SELECT 1 + 1 as sum").fetch_one(&pool).await?;
+    sqlx::migrate!("./migrations").run(&pool).await?;
 
-    let sum: i32 = res.get("sum");
-    println!("1 + 1 = {}", sum);
+    let sub = Sub {
+        email: "vaarischitkara16@gmail.com".to_string(),
+        name: "vaaris".to_string(),
+    };
+
+    create_subscriber(&sub, &pool).await?;
+
     Ok(())
 }
