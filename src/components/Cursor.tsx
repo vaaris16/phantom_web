39babdeda071858da97be29dd92542b6d0e9a695
Vaaris Chitkara <vaarischitkara16@gmail.com>
2026-03-27 14:51:22 +0530

    changed cursor size

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -36,8 +36,8 @@ export default function CursorEffect() {
       ref={cursorRef}
       className="fixed top-0 left-0 pointer-events-none z-9999 will-change-transform -translate-x-1/2 -translate-y-1/2"
     >
-      <div className="absolute left-1/2 top-1/2 w-20 h-20 blur-xl -translate-x-1/2 -translate-y-1/2 rounded-full bg-(--neon-pink) opacity-100" />
-      <div className="absolute left-1/2 top-1/2 w-14 h-14  blur-xl -translate-x-1/2 -translate-y-1/2 rounded-full bg-blue-950 opacity-100" />
+      <div className="absolute left-1/2 top-1/2 w-14 h-14 blur-xl -translate-x-1/2 -translate-y-1/2 rounded-full bg-(--neon-pink) opacity-100" />
+      <div className="absolute left-1/2 top-1/2 w-10 h-10  blur-xl -translate-x-1/2 -translate-y-1/2 rounded-full bg-blue-950 opacity-100" />
 
       <div className="absolute left-1/2 top-1/2 w-2 h-2  -translate-x-1/2 -translate-y-1/2 rounded-full bg-white opacity-100" />
       <div className="absolute left-1/2 top-1/2 w-4 h-4  blur-sm -translate-x-1/2 -translate-y-1/2 rounded-full bg-(--cyan) opacity-100" />
